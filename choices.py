816d05b54e98751fd698e06ba2f3f25fb7b83852
2F <44865237+F-a-b-r-i-z-i-o@users.noreply.github.com>
2022-02-27 16:11:59 +0100

    Add files via upload

---
+++ choices.py
@@ -0,0 +1,224 @@
+import utils
+import sys
+from map_env import LabyrinthEnv
+from game import Game
+from q_learning import QLearning
+from level import *
+
+
+def main_menu():
+    choice = '0'
+    while choice == '0':
+        print("")
+        print("Choose the level you want to play:\n")
+        print("(1)\tMap 10 x 10\n")
+        print("(2)\tMap 20 x 20\n")
+        print("(3)\tMap 30 x 30\n")
+        print("(4)\tExit Game\n")
+
+        choice = input("\tPlease make a choice:\n ")
+
+        if choice == "1":
+            print("-----------------------------------------")
+            print("\tLEVEL 1 SELECTED\n")
+            print("-----------------------------------------")
+            lev1_menu()
+        elif choice == "2":
+            print("-----------------------------------------")
+            print("\tLEVEL 2 SELECTED\n")
+            print("-----------------------------------------")
+            lev2_menu()
+        elif choice == "3":
+            print("-----------------------------------------")
+            print("\tLEVEL 3 SELECTED\n")
+            print("-----------------------------------------")
+            lev3_menu()
+        elif choice == "4":
+            print("-----------------------------------------")
+            print("\tEXIT GAME\n")
+            print("-----------------------------------------")
+            sys.exit()
+        else:
+            print("I don't understand your choice.")
+
+
+def lev1_menu():
+    wall_input = input(
+        "Choose the percentage of the wall:\n"
+        "(1)\tLow\n"
+        "(2)\tMedium\n"
+        "(3)\tHigh\n"
+    )
+
+    env = None
+
+    if wall_input == "1":
+        max_actions = int(input("\tMax actions:\n"))
+        game = Game(lev_1_easy, 'x', 'p', 'e')
+        game.create_game()
+        env = LabyrinthEnv(max_actions, game=game, map_h=10, map_w=10)
+
+    elif wall_input == "2":
+        max_actions = int(input("\tMax actions:\n"))
+        game = Game(lev_1_medium, 'x', 'p', 'e')
+        game.create_game()
+        env = LabyrinthEnv(max_actions, game=game, map_h=10, map_w=10)
+
+    elif wall_input == "3":
+        max_actions = int(input("\tMax actions:\n"))
+        game = Game(lev_1_hard, 'x', 'p', 'e')
+        game.create_game()
+        env = LabyrinthEnv(max_actions, game=game, map_h=10, map_w=10)
+    else:
+        print("End\n")
+        sys.exit()
+
+    QL = QLearning(env)
+    utils.clear_screen()
+
+    env.render()
+    menu_input = input(
+        "(1)\tTraining\n"
+        "(2)\tExecute automatically\n"
+        "(3)\tExecute step-by-step\n"
+        "(0)\tExit\n"
+    )
+    utils.clear_screen()
+
+    if menu_input == "1":
+        print("\tTraining\n")
+        QL.training(epochs=20000, steps=200, alpha=0.1,
+                    gamma=1.0, eps=1.0, plot=True)
+
+    elif menu_input == "2":
+        print("\tExecute\n")
+        QL.execute(step_by_step=False)
+
+    elif menu_input == "3":
+        print("\tExecute\n")
+        QL.execute(step_by_step=True)
+
+    else:
+        print("End\n")
+
+
+def lev2_menu():
+    wall_input = input(
+        "Choose the percentage of the wall:\n"
+        "(1)\tLow\n"
+        "(2)\tMedium\n"
+        "(3)\tHigh\n"
+    )
+
+    env = None
+
+    if wall_input == "1":
+        max_actions = int(input("\tMax actions:\n"))
+        game = Game(lev_2_easy, 'x', 'p', 'e')
+        game.create_game()
+        env = LabyrinthEnv(max_actions, game=game, map_h=20, map_w=20)
+
+    elif wall_input == "2":
+        max_actions = int(input("\tMax actions:\n"))
+        game = Game(lev_2_medium, 'x', 'p', 'e')
+        game.create_game()
+        env = LabyrinthEnv(max_actions, game=game, map_h=20, map_w=20)
+
+    elif wall_input == "3":
+        max_actions = int(input("\tMax actions:\n"))
+        game = Game(lev_2_hard, 'x', 'p', 'e')
+        game.create_game()
+        env = LabyrinthEnv(max_actions, game=game, map_h=20, map_w=20)
+
+    else:
+        print("End\n")
+        sys.exit()
+
+    QL = QLearning(env)
+    utils.clear_screen()
+
+    env.render()
+    menu_input = input(
+        "(1)\tTraining\n"
+        "(2)\tExecute automatically\n"
+        "(3)\tExecute step-by-step\n"
+        "(0)\tExit\n"
+    )
+    utils.clear_screen()
+
+    if menu_input == "1":
+        print("\tTraining\n")
+        QL.training(epochs=20000, steps=200, alpha=0.1,
+                    gamma=1.0, eps=1.0, plot=True)
+
+    elif menu_input == "2":
+        print("\tExecute\n")
+        QL.execute(step_by_step=False)
+
+    elif menu_input == "3":
+        print("\tExecute\n")
+        QL.execute(step_by_step=True)
+
+    else:
+        print("\tEnd\n")
+
+
+def lev3_menu():
+    wall_input = input(
+        "Choose the percentage of the wall:\n"
+        "(1)\tLow\n"
+        "(2)\tMedium\n"
+        "(3)\tHigh\n"
+    )
+
+    env = None
+
+    if wall_input == "1":
+        max_actions = int(input("\tMax actions: "))
+        game = Game(lev_3_easy, 'x', 'p', 'e')
+        game.create_game()
+        env = LabyrinthEnv(max_actions, game=game, map_h=30, map_w=30)
+
+    elif wall_input == "2":
+        max_actions = int(input("\tMax actions: "))
+        game = Game(lev_3_medium, 'x', 'p', 'e')
+        game.create_game()
+        env = LabyrinthEnv(max_actions, game=game, map_h=30, map_w=30)
+
+    elif wall_input == "3":
+        max_actions = int(input("\tMax actions: "))
+        game = Game(lev_3_hard, 'x', 'p', 'e')
+        game.create_game()
+        env = LabyrinthEnv(max_actions, game=game, map_h=30, map_w=30)
+
+    else:
+        print("End\n")
+        sys.exit()
+
+    QL = QLearning(env)
+    utils.clear_screen()
+
+    env.render()
+    menu_input = input(
+        "(1)\tTraining\n"
+        "(2)\tExecute automatically\n"
+        "(3)\tExecute step-by-step\n"
+        "(0)\tExit\n"
+    )
+    utils.clear_screen()
+
+    if menu_input == "1":
+        print("Training\n")
+        QL.training(epochs=20000, steps=200, alpha=0.1,
+                    gamma=1.0, eps=1.0, plot=True)
+
+    elif menu_input == "2":
+        print("Execute\n")
+        QL.execute(step_by_step=False)
+
+    elif menu_input == "3":
+        print("Execute\n")
+        QL.execute(step_by_step=True)
+
+    else:
+        print("End\n")
---
+++ q_learning.py
@@ -0,0 +1,172 @@
+import numpy as np
+import time
+import map_env
+import matplotlib.pyplot as plt
+
+
+class QLearning(object):
+    def __init__(self, env):
+        self.env = env
+        self.obs = None
+        self.tot_reward = 0
+
+    def max_action(self, q, state, actions):
+        """
+        Returns the actions with highest value in the Q matrix for a given state
+        """
+        values = np.array([q[state, a] for a in actions])
+        action = np.argmax(values)
+        return actions[action]
+
+    def save_q(self, q, file_name):
+        """
+        Saves the Q matrix to a file
+        """
+        num_states = len(self.env.state_space)
+        num_actions = len(self.env.possible_actions)
+        q_matrix = np.zeros((num_states, num_actions))
+        x = 0
+        for state in self.env.state_space:
+            y = 0
+            for action in self.env.possible_actions:
+                q_matrix[x][y] = q[state, action]
+                y += 1
+            x += 1
+        np.savetxt(file_name, q_matrix, delimiter=" ")
+
+    def load_q(self, file_name):
+        """
+        Loads the Q matrix from a file
+        """
+        q = {}
+        for state in self.env.state_space:
+            for action in self.env.possible_actions:
+                q[state, action] = 0
+        with open(file_name) as file_name:
+            q_matrix = np.loadtxt(file_name, delimiter=" ")
+
+        x = 0
+        for state in self.env.state_space:
+            y = 0
+            for action in self.env.possible_actions:
+                q[state, action] = q_matrix[x][y]
+                y += 1
+            x += 1
+        return q
+
+    def execute(self, step_by_step=False):
+        """
+        Executes the Q-Learning algorithm from the matrix saved step by step
+        """
+        self.env.reset()
+        self.env.render()
+        q = self.load_q("Qmatrix")
+        tot_reward = 0
+
+        command = ""
+        print("Command List")
+        while command != "n":
+            if step_by_step:
+                print("------------------------")
+                print("Turn left: l")
+                print("Turn right: r")
+                print("Up: u")
+                print("Down: d")
+                print("------------------------")
+                command = input()
+            else:
+                time.sleep(0.5)
+
+            _, self.obs = self.env.next_observation()
+
+            if command == "u":
+                action = map_env.possible_actions["U"]
+            elif command == "d":
+                action = map_env.possible_actions["D"]
+            elif command == "l":
+                action = map_env.possible_actions["L"]
+            elif command == "r":
+                action = map_env.possible_actions["R"]
+            else:  # y
+                action = self.max_action(
+                    q, self.obs, self.env.possible_actions)
+
+            observation_next, reward, done, info = self.env.step(
+                action, draw=True)
+            self.tot_reward += reward
+
+            self.env.render()
+            print("Action:\t" + map_env.actions_id[action])
+            print("Reward:\t{}".format(reward))
+            print("Return:\t{}".format(str(self.tot_reward)))
+            print("")
+
+            if done:
+                print("The Agent has reached the exit")
+
+    def training(
+        self, epochs=25000, steps=200, alpha=0.1, gamma=1.0, eps=1.0, plot=True
+    ):
+        """
+        Trains the Q-Learning Algorithm and saves the Q matrix built
+        Default hyperparameters
+            alpha = 0.1 is the learning rate of q learning
+            gamma = 1.0 is the discount factor
+            eps = 1.0   is the epsilon and is related to the greedy value of the algorithm
+            epochs = 50000 is the max number of epochs
+            steps = 200 is the max number of actions ( step ) per epoch
+        """
+        # initializing Q(state, action) matrix to zero
+        q = {}
+        for state in self.env.state_space:
+            for action in self.env.possible_actions:
+                q[state, action] = 0
+
+        self.env.reset()
+        self.env.render()
+
+        total_rewards = np.zeros(epochs)
+        start_time = time.time()
+
+        for i in range(epochs):
+            if i % int(epochs / 10) == 0:
+                print("epochs passed: ", i)
+
+            done = False
+            ep_rewards = 0
+            num_actions = 0
+            observation = self.env.reset()
+            while not done and num_actions <= steps:
+                rand = np.random.random()
+                action = (
+                    self.max_action(q, observation, self.env.possible_actions)
+                    if rand < (1 - eps)
+                    else self.env.action_space_sample()
+                )
+                observation_next, reward, done, info = self.env.step(action)
+                num_actions += 1
+                ep_rewards += reward
+                action_next = self.max_action(
+                    q, observation_next, self.env.possible_actions
+                )
+                q[observation, action] = q[observation, action] + alpha * (
+                    reward
+                    + gamma * q[observation_next, action_next]
+                    - q[observation, action]
+                )
+                observation = observation_next
+            if eps - 2 / epochs > 0:
+                eps -= 2 / epochs
+            else:
+                eps = 0
+            total_rewards[i] = ep_rewards
+
+        end_time = time.time()
+
+        if plot:
+            plt.plot(total_rewards)
+            plt.show()
+
+        self.save_q(q, "Qmatrix")
+
+        print("execution time = {:.2f} seconds".format(end_time - start_time))
